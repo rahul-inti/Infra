6ba5e88d3e2c8d8ef5e023ae011b777e85816edb
Inti Rahul - (Digital) <in20071139@wipro.com>
2020-03-17 12:07:18 +0530

    d

--- infra.tf
+++ infra.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "${var.region}"
-  access_key = "$AWS_ACCESS_KEY_ID"
-  secret_key = "$AWS_SECRET_ACCESS_KEY"
+  access_key = "${AWS_ACCESS_KEY_ID}"
+  secret_key = "${AWS_SECRET_ACCESS_KEY}"
 }
 
 resource "aws_vpc" "prod" {
