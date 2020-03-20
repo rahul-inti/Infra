3f83eb2a096bdb8f739bf5c00df20a02596fb178
Inti Rahul - (Digital) <in20071139@wipro.com>
2020-03-20 17:52:43 +0530

    d

--- variable.tf
+++ variable.tf
@@ -28,13 +28,5 @@ variable "hostnames" {
   default = "true"
 }
 
-variable "Public" {
-  default = ["10.0.0.0/24", "10.0.1.0/24"]
-}
-
-variable "Private" {
-  default = ["10.0.2.0/24"]
-}
-
 # Declare the data source
 data "aws_availability_zones" "azs" {}
