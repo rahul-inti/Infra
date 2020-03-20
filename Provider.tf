808a4b64abfd0beb4bb951838afc8bb60b371c44
Inti Rahul - (Digital) <in20071139@wipro.com>
2020-03-20 11:20:20 +0530

    s

--- Provider.tf
+++ Provider.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "${var.region}"
-  assume_role {
-    role_arn = "arn:aws:iam::141570103273:role/Terraform"
-  }
+
 }
+assume_role {
+  role_arn = "arn:aws:iam::141570103273:role/Terraform"
+  }
