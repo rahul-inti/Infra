4324cbb0b127938296e246284392a29385146403
Inti Rahul - (Digital) <in20071139@wipro.com>
2020-03-21 19:20:44 +0530

    v

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "dominose" {
   ami           = "${var.images}" # us-west-2
   instance_type = "${var.ec2}"
-
+  region = "${var.region}"
 }
--- variable.tf
+++ variable.tf
@@ -37,15 +37,5 @@ variable "images" {
   }
 }
 
-variable "ec2" {
-  type = "map"
-
-  default = {
-    us-east-1 = "t2.micro"
-    us-west-2 = "t2.micro"
-}
-}
-
-
 # Declare the data source
 data "aws_availability_zones" "azs" {}
